1a92f1a7b65cc0d86bbbf2165e9b1bbb7e284739
bobo-the-dev <>
2022-12-16 00:20:19 +0700

    add nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import packageJson from '../../package.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'note-app-ui';
+  public version: string = packageJson.version;
 }
